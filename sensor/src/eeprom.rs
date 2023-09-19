996655dd0baf18f4b422bb6b6a9cdffa596555b4
Louis Moreau <elixar05@gmail.com>
2023-09-19 23:52:32 +0200

    continued to work on embedded response , should work now but it's not tested yet

--- sensor/src/eeprom.rs
+++ sensor/src/eeprom.rs
@@ -27,39 +27,41 @@ const SENSOR_ID_SIZE: u32 = 8;
 const EVENT_ADDRESS: u32 = SENSOR_ID_ADDRESS + SENSOR_ID_SIZE;
 const EVENT_SIZE: u32 = Event::POSTCARD_MAX_SIZE as u32;
 
-pub fn write_event_to_eeprom(eeprom: &mut MyEeprom, event: Event) {
-    let count = read_count_from_eeprom(eeprom);
+pub fn write_event_to_eeprom(eeprom: &mut MyEeprom, event: Event) ->  Result<(),()> {
+    let count = read_count_from_eeprom(eeprom)?;
     let event_data = match event.to_bytes() {
         Ok(e) => e,
-        Err(_) => return,
+        Err(_) => return Err(()),
     };
-    write_data_to_eeprom_blocking(eeprom, get_event_adress(count), &event_data);
-    increment_stored_count(eeprom);
+    write_data_to_eeprom_blocking(eeprom, get_event_adress(count), &event_data)?;
+    increment_stored_count(eeprom)?;
+    Ok(())
 }
 
-pub fn read_event_from_eeprom(eeprom: &mut MyEeprom, number: u32) -> Event {
+pub fn read_event_from_eeprom(eeprom: &mut MyEeprom, number: u32) -> Result<Event,()> {
     let mut data = [0u8; EVENT_SIZE as usize];
     eeprom
         .read_data(get_event_adress(number), &mut data)
-        .unwrap();
+        .map_err(|_|())?;
 
     match Event::from_bytes(&data) {
-        Ok(e) => e,
-        Err(_) => Event::Err,
+        Ok(e) => Ok(e),
+        Err(_) => Ok(Event::Err),
     }
 }
 
-pub fn increment_stored_count(eeprom: &mut MyEeprom) {
-    let mut count = read_count_from_eeprom(eeprom);
+pub fn increment_stored_count(eeprom: &mut MyEeprom) ->  Result<(),()> {
+    let mut count = read_count_from_eeprom(eeprom)?;
     count += 1;
     rprintln!("incremented count : {}", count);
-    write_data_to_eeprom_blocking(eeprom, COUNT_ADDRESS, &count.to_le_bytes());
+    write_data_to_eeprom_blocking(eeprom, COUNT_ADDRESS, &count.to_le_bytes())?;
+    Ok(())
 }
 
-pub fn read_count_from_eeprom(eeprom: &mut MyEeprom) -> u32 {
+pub fn read_count_from_eeprom(eeprom: &mut MyEeprom) -> Result<u32,()> {
     let mut data = [0u8; COUNT_SIZE as usize];
-    eeprom.read_data(COUNT_ADDRESS, &mut data).unwrap();
-    u32::from_le_bytes(data)
+    eeprom.read_data(COUNT_ADDRESS, &mut data).map_err(|_|())?;
+    Ok(u32::from_le_bytes(data))
 }
 
 pub fn write_data_to_eeprom_blocking(eeprom: &mut MyEeprom, address: u32, data: &[u8]) ->  Result<(),()> {
@@ -83,19 +85,20 @@ pub fn write_byte_to_eeprom_blocking(eeprom: &mut MyEeprom, address: u32, byte: 
     })
 }
 
-pub fn zero_stored_count(eeprom: &mut MyEeprom) {
+pub fn zero_stored_count(eeprom: &mut MyEeprom) -> Result<(),()> {
     let count: u32 = 0;
-    write_data_to_eeprom_blocking(eeprom, COUNT_ADDRESS, &count.to_le_bytes());
+    write_data_to_eeprom_blocking(eeprom, COUNT_ADDRESS, &count.to_le_bytes()).map_err(|_|())?;
+    Ok(())
 }
 
-pub fn write_sensor_id(eeprom: &mut MyEeprom, sensor_id: u64) {
-    write_data_to_eeprom_blocking(eeprom, SENSOR_ID_ADDRESS, &sensor_id.to_le_bytes());
+pub fn write_sensor_id(eeprom: &mut MyEeprom, sensor_id: u64) ->  Result<(),()> {
+    write_data_to_eeprom_blocking(eeprom, SENSOR_ID_ADDRESS, &sensor_id.to_le_bytes())
 }
 
-pub fn read_sensor_id(eeprom: &mut MyEeprom) -> u64 {
+pub fn read_sensor_id(eeprom: &mut MyEeprom) -> Result<u64,()> {
     let mut data = [0u8; SENSOR_ID_SIZE as usize];
-    eeprom.read_data(SENSOR_ID_ADDRESS, &mut data).unwrap();
-    u64::from_le_bytes(data)
+    eeprom.read_data(SENSOR_ID_ADDRESS, &mut data).map_err(|_|())?;
+    Ok(u64::from_le_bytes(data))
 }
 
 fn get_event_adress(count: u32) -> u32 {
--- sensor/src/tasks/uart_interrupt.rs
+++ sensor/src/tasks/uart_interrupt.rs
@@ -1,6 +1,7 @@
 use chrono::NaiveDateTime;
-use cortex_m::prelude::_embedded_hal_serial_Read;
-use link_lib::{MessageBuffer, Response};
+use stm32l0xx_hal::prelude::_embedded_hal_serial_Read;
+use stm32l0xx_hal::prelude::_embedded_hal_serial_Write;
+use link_lib::*;
 use nb::block;
 use rtic::Mutex;
 use stm32l0xx_hal::{
@@ -8,7 +9,7 @@ use stm32l0xx_hal::{
     serial::*,
 };
 
-use crate::{app::uart0, eeprom::read_sensor_id};
+use crate::{app::uart0, eeprom::{read_sensor_id, write_sensor_id, zero_stored_count, read_count_from_eeprom, read_event_from_eeprom}};
 
 pub fn uart_interrupt(mut ctx: uart0::Context) {
     if Exti::is_pending(DirectLine::Lpuart1) {
@@ -24,7 +25,10 @@ pub fn uart_interrupt(mut ctx: uart0::Context) {
         }
 
         while ctx.local.uart_int_state.uart_rx.is_rx_not_empty() {
-            let byte = block!(ctx.local.uart_int_state.uart_rx.read()).unwrap();
+            let byte = match block!(ctx.local.uart_int_state.uart_rx.read()) {
+                Ok(b) => b,
+                Err(_) => break,
+            };
             if let Ok(Some(req)) = ctx
                 .local
                 .uart_int_state
@@ -37,18 +41,30 @@ pub fn uart_interrupt(mut ctx: uart0::Context) {
                         Response::EmbeddedTime(ctx.shared.rtc.lock(|r| r.now().timestamp() as u32))
                     }
                     link_lib::Request::SetEmbeddedTime(t) => set_time(&mut ctx, t),
-                    link_lib::Request::GetSensorId => {
-                        Response::SensorId(ctx.shared.eeprom.lock(|eeprom| read_sensor_id(eeprom)))
-                    }
-                    link_lib::Request::SetSensorId(id) => todo!(),
-                    link_lib::Request::ClearMemory => todo!(),
-                    link_lib::Request::GetNumberofEvent => todo!(),
-                    link_lib::Request::GetEvent(n) => todo!(),
+                    link_lib::Request::GetSensorId => get_sensor_id(&mut ctx),
+                    link_lib::Request::SetSensorId(id) => set_sensor_id(&mut ctx,id),
+                    link_lib::Request::ClearMemory => clear_memory(&mut ctx),
+                    link_lib::Request::GetNumberofEvent => get_number_of_event(&mut ctx),
+                    link_lib::Request::GetEvent(n) => get_event(&mut ctx,n),
                 };
+                ctx.local.uart_int_state.message_buffer.clear_buffer();
+                let response_bytes = match Response::encode_message(&response) {
+                    Ok(b) => b,
+                    Err(_) => continue,
+                };
+                for byte in response_bytes {
+                    match block!(ctx.local.uart_int_state.uart_tx.write(byte)) {
+                        Ok(_) => (),
+                        Err(_) => break,
+                    };
+                }
+
             }
         }
     }
 }
+
+
 
 fn set_time(ctx: &mut uart0::Context, timestamp: u32) -> Response {
     let new_time = match NaiveDateTime::from_timestamp_opt(timestamp as i64, 0) {
@@ -60,6 +76,41 @@ fn set_time(ctx: &mut uart0::Context, timestamp: u32) -> Response {
         Ok(_) => Response::Ok,
         Err(_) => Response::Error,
     };
+}
+
+fn get_sensor_id(ctx: &mut uart0::Context) -> Response {
+    match ctx.shared.eeprom.lock(|eeprom| read_sensor_id(eeprom)) {
+        Ok(id) => Response::SensorId(id),
+        Err(_) => Response::Error,
+    }
+}
+
+fn set_sensor_id(ctx: &mut uart0::Context, sensor_id: u64) -> Response {
+    match ctx.shared.eeprom.lock(|eeprom| write_sensor_id(eeprom, sensor_id)) {
+        Ok(_) => Response::Ok,
+        Err(_) => Response::Error,
+    }
+}
+
+fn clear_memory(ctx: &mut uart0::Context) -> Response {
+    match ctx.shared.eeprom.lock(|eeprom|zero_stored_count(eeprom)) {
+        Ok(_) => Response::Ok,
+        Err(_) => Response::Error,
+    }
+}
+
+fn get_number_of_event(ctx: &mut uart0::Context) -> Response {
+    match ctx.shared.eeprom.lock(|eeprom|read_count_from_eeprom(eeprom)) {
+        Ok(_) => Response::Ok,
+        Err(_) => Response::Error,
+    }
+}
+
+fn get_event(ctx: &mut uart0::Context, number : u32) -> Response {
+    match ctx.shared.eeprom.lock(|eeprom|read_event_from_eeprom(eeprom,number)) {
+        Ok(e) => Response::EventInfo(e),
+        Err(_) => Response::Error,
+    }
 }
 
 pub struct UartInterruptState {
